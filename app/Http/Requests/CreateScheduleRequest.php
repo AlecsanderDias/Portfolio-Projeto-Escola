225ab4772f6a1de7bfbd6bbda39670a3006a20f6
Alecsander <alecsanderdias@hotmail.com>
2025-09-02 23:37:58 -0300

    Criando Seeder e Factory de Cronograma

--- app/Http/Requests/CreateScheduleRequest.php
+++ app/Http/Requests/CreateScheduleRequest.php
@@ -25,8 +25,8 @@ class CreateScheduleRequest extends FormRequest
             'day' => 'required|string',
             'hour' => 'required|string',
             'semester' => 'required|string|between:1,2',
-            'subject' => 'required|integer',
-            'school_class' => 'required|integer',
+            'subject_id' => 'required|integer',
+            'school_class_id' => 'required|integer',
         ];
     }
 
